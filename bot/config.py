8252073f7a3052f1bb7f8b9c1f3c4ed02b5845e9
Akshad499 <163293218+Akshad499@users.noreply.github.com>
2024-03-13 16:18:16 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,11 +4,11 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("25271554", cast=int)
+    API_HASH = config("cf9936ecab34ec6f25be13dd4bca3eaa")
+    BOT_TOKEN = config("6621913506:AAFmQjHVpAhVhPfFVcKc4utnFK6j9-miVbw")
     DEV = 1664850827
-    OWNER = config("OWNER")
+    OWNER = config("5236678934")
     ffmpegcode = ["-map 0 -c:v libx264 -pix_fmt yuv420p -crf 30 -preset veryfast -s 856x480 -c:a aac -b:a 50k -c:s copy"]
     THUMBNAIL = config("THUMBNAIL", default="https://telegra.ph/BREEZE-HERE-02-11")
 except Exception as e:
